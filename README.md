398b74da156e1d38f0f2ef0f52f2f6358cd1547c
Johnny Magrippis <j@magrippis.com>
2023-09-19 14:05:32 +0200

    🚀 initial commit with bun setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sverdle
+
+![E2E](https://github.com/jmagrippis/sverdle/actions/workflows/playwright.yaml/badge.svg)
+
+Demo project for a SvelteKit implementation of Wordle... with [bun](https://bun.sh/)!
---
+++ playwright.config.ts
@@ -0,0 +1,75 @@
+import {devices, type PlaywrightTestConfig} from '@playwright/test'
+
+const config: PlaywrightTestConfig = {
+	/* Run tests in files in parallel */
+	fullyParallel: true,
+	/* Fail the build on CI if you accidentally left test.only in the source code. */
+	forbidOnly: !!process.env.CI,
+	/* Retry on CI only */
+	retries: process.env.CI ? 2 : 0,
+	/* Opt out of parallel tests on CI. */
+	workers: process.env.CI ? 1 : undefined,
+	webServer: {
+		command: 'bun run build && bun run preview',
+		port: 4173,
+		reuseExistingServer: !process.env.CI,
+	},
+	testDir: 'tests',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	use: {
+		/* Base URL to use in actions like `await page.goto('/')`. */
+		baseURL: 'http://localhost:4173',
+
+		launchOptions: {
+			slowMo: parseInt(process.env.SLOW_MO || '0'),
+		},
+
+		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		trace: 'on-first-retry',
+		video: 'retain-on-failure',
+	},
+
+	/* Configure projects for major browsers */
+	projects: [
+		{name: 'setup', testMatch: /.*\.setup\.ts/},
+		{
+			name: 'chromium',
+			use: {...devices['Desktop Chrome']},
+			dependencies: ['setup'],
+		},
+
+		{
+			name: 'firefox',
+			use: {...devices['Desktop Firefox']},
+			dependencies: ['setup'],
+		},
+
+		// {
+		//   name: 'webkit',
+		//   use: { ...devices['Desktop Safari'] },
+		// },
+
+		/* Test against mobile viewports. */
+		// {
+		//   name: 'Mobile Chrome',
+		//   use: { ...devices['Pixel 5'] },
+		// },
+		{
+			name: 'Mobile Safari',
+			use: {...devices['iPhone 12']},
+			dependencies: ['setup'],
+		},
+
+		/* Test against branded browsers. */
+		// {
+		//   name: 'Microsoft Edge',
+		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+		// },
+		// {
+		//   name: 'Google Chrome',
+		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+		// },
+	],
+}
+
+export default config
